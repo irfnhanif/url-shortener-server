96332bf9d2f3ae68c38e3cba43c983d040ed9bbe
irfnhanif <14.irfanh@gmail.com>
2025-03-16 19:58:55 +0700

    Prepare project

---
+++ src/test/java/io/github/irfnhanif/urlshorterner/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.irfnhanif.urlshorterner.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
