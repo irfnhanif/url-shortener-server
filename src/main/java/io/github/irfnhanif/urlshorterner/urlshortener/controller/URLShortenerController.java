6001399ec1bd4400997f7c6f583e70c62dcd355b
irfnhanif <14.irfanh@gmail.com>
2025-04-07 14:10:23 +0700

    Add Redis configuration and repository for URL mappings

---
+++ src/main/java/io/github/irfnhanif/urlshorterner/urlshortener/controller/URLShortenerController.java
@@ -0,0 +1,16 @@
+package io.github.irfnhanif.urlshorterner.urlshortener.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class URLShortenerController {
+
+    @GetMapping("/short-url")
+    public ResponseEntity<> getRedirectURL(@RequestParam String param) {
+        return new String();
+    }
+
+}
